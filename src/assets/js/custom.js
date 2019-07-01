617d66eea8749ce2032104f024771c09f1a05e0c
Dhivakar <dhivakar.kuppusamy@colanonline.com>
2019-07-01 19:20:40 +0530

    history page added and paid green icon added

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -112,4 +112,13 @@ $(document).on("click", function () {
     $(".dropdown-list-search2").hide();
 });
 
+$(document).ready(function () {
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).parent().addClass('active');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).parent().removeClass('active');
+    });
+});
 
